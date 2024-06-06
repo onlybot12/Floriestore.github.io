676545839d9d219791d7c5d7dfc9e262c2662c08
Maulana14 <116287549+onlybot12@users.noreply.github.com>
2024-06-06 19:20:37 +0700

    Create aov.js

---
+++ assets/js/aov.js
@@ -0,0 +1,42 @@
+const harga = document.getElementById("total")
+const total = document.getElementById("total1")
+const pay = document.getElementById("pembayaran")
+let wallet = document.querySelectorAll("p.wallet3")
+
+
+function tp1(){
+  for (let i = 0; i < wallet.length; i++) {
+  pay.scrollIntoView();
+  harga.style.display = "block";
+  total.innerHTML = "Rp. 19.500";
+  // payment nominal
+  wallet[i].innerHTML = "Rp. 19.500";
+  }
+}
+function tp2(){
+  for (let i = 0; i < wallet.length; i++) {
+  pay.scrollIntoView();
+  harga.style.display = "block";
+  total.innerHTML = "Rp. 95.000";
+  // payment nominal
+  wallet[i].innerHTML = "Rp. 95.000";
+  }
+}
+function tp3(){
+  for (let i = 0; i < wallet.length; i++) {
+  pay.scrollIntoView();
+  harga.style.display = "block";
+  total.innerHTML = "Rp. 285.000";
+  // payment nominal
+  wallet[i].innerHTML = "Rp. 285.000";
+  }
+}
+function tp4(){
+  for (let i = 0; i < wallet.length; i++) {
+  pay.scrollIntoView();
+  harga.style.display = "block";
+  total.innerHTML = "Rp. 950.000";
+  // payment nominal
+  wallet[i].innerHTML = "Rp. 950.000";
+  }
+}
